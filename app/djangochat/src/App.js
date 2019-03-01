9babfe7267ac12161e387583454fb052197ee70b
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-03-01 15:21:47 +0100

    add bootstrap

--- app/djangochat/src/App.js
+++ app/djangochat/src/App.js
@@ -2,9 +2,57 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 import AuthPage from './pages/Auth';
 import HomePage from './pages/Home';
 import ChatPage from './pages/Chat';
+
+// import { createStore } from 'redux';
+
+// const userReducer = (state = {
+//   id: null,
+//   username: null,
+//   email: null,
+//   token: null
+// }, action) => {
+//   switch(action.type){
+//     case "ADD":
+//       state = {
+//         ...state,
+//         id : action.id
+//       };
+//       break;
+//     case "REFRESH_TOKEN":
+//       state = {
+//         ...state,
+//         token: action.token
+//       };
+//   }
+//   return state;
+// };
+
+// const serverReducer = (state, action) => {
+//   switch(action.type){
+//     case "ADD":
+//       state = state + action.value;
+//       break;
+//   }
+//   return state;
+// };
+
+// const channelReducer = (state, action) => {
+//   switch(action.type){
+//     case "ADD":
+//       state = state + action.value;
+//       break;
+
+//     case "SUBSTRACT":
+//       break;
+//   }
+//   return state;
+// };
+
+// const store = createStore(,1);
 
 class App extends Component {
   render() {
