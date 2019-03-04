260594f118e05b54db951a90c7e52f5ddd7e6060
Etiouse <etienne.husler@he-arc.ch>
2019-03-04 17:55:29 +0100

    Dynamic connection status in local

--- app/djangochat/src/components/MainPane/LeftPane/UserSettings/UserSettings.js
+++ app/djangochat/src/components/MainPane/LeftPane/UserSettings/UserSettings.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
+import './UserSettings.css';
 
 class LeftPane extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            activity: "Connected",
+            time: 0,
+            delay: 10000
+        };
+        this.startTimer = this.startTimer.bind(this)
+        this.checkActivity = this.checkActivity.bind(this);
+        this.openSettings = this.openSettings.bind(this);
+
+        this.startTimer();
+    }
+
+    startTimer() {
+        this.state.time = Date.now();
+        setInterval(this.checkActivity, 1000);
+    }
+
+    checkActivity() {
+        if (Date.now() - this.state.time > this.state.delay)
+        {
+            this.setState({
+                activity: "Away"
+            })
+        }
     }
     
     openSettings() {
-        // TODO
-        alert("You need to implement user settings first");
+        alert(this.state.activity);
     }
 
     render() {
+        var connectionImg;
+        switch (this.state.activity)
+        {
+            case "Away":    
+                connectionImg = require('./images/away.png');
+                break;
+            default:
+                connectionImg = require('./images/connected.png');
+        }
         return (
             <div className="row" style={{height: '100%', textAlign: 'center'}}>
-                <div className="col" style={{paddingTop: '20px', paddingRight: '0',}}>
-                    <img src={require('./images/profile.png')} width="70" height="70"/>
+                <div className="col-3" style={{paddingTop: '20px', paddingRight: '0', paddingLeft: '30px'}}>
+                    <img alt="" src={require('./images/profile.png')} width="70" height="70"/>
+                    <img id="connection" alt="" src={connectionImg}/>
                 </div>
-                <div className="col" style={{paddingTop: '35px', paddingLeft: '0', fontSize: '1.5em', color: '#000000'}}>
+                <div className="col-6" style={{paddingTop: '35px', paddingLeft: '10px', fontSize: '1.5em', color: '#000000'}}>
                     Username
                 </div>
-                <div className="col" style={{paddingTop: '35px', paddingLeft: '0'}}>
-                    <img src={require('./images/settings.png')} width="40" height="40" onClick={this.openSettings}/>
+                <div className="col-3" style={{paddingTop: '35px', paddingLeft: '0'}}>
+                    <img alt="" src={require('./images/settings.png')} width="40" height="40" onClick={this.openSettings}/>
                 </div>
             </div>
         );
