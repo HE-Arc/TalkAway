aec16bfce775fa86abddb1dcd757bb35096e55f2
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-03-05 22:27:08 +0100

    message mutation

--- api/djangochat/api/schema.py
+++ api/djangochat/api/schema.py
@@ -5,34 +5,43 @@ from graphene_django.types import DjangoObjectType
 
 from .models import Message, Server, Channel, Friend, Right, Reaction, User
 
+from django.contrib.auth import get_user_model
+
 
 class MessageType(DjangoObjectType):
     class Meta:
         model = Message
 
+
 class ServerType(DjangoObjectType):
     class Meta:
         model = Server
+
 
 class ChannelType(DjangoObjectType):
     class Meta:
         model = Channel
 
+
 class FriendType(DjangoObjectType):
     class Meta:
         model = Friend
 
+
 class RightType(DjangoObjectType):
     class Meta:
         model = Right
-        
+
+
 class ReactionType(DjangoObjectType):
     class Meta:
         model = Reaction
-        
+
+
 class UserType(DjangoObjectType):
     class Meta:
         model = User
+
 
 class Query(object):
     all_users = graphene.List(UserType)
@@ -41,7 +50,7 @@ class Query(object):
     all_friends = graphene.List(FriendType)
     all_rights = graphene.List(RightType)
     all_reactions = graphene.List(ReactionType)
-    all_users = graphene.List(UserType)
+    all_messages = graphene.List(MessageType)
 
     def resolve_all_channels(self, info, **kwargs):
         return Channel.objects.all()
@@ -51,6 +60,33 @@ class Query(object):
 
     def resolve_all_servers(self, info, **kwargs):
         return Server.objects.all()
+
+    def resolve_all_messages(self, info, **kwargs):
+        return Message.objects.all()
+
+    def resolve_all_users(self, info, **kwargs):
+        return get_user_model().objects.all()
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(
+            user=user,
+        )
 
 
 class CreateServer(graphene.Mutation):
@@ -72,5 +108,32 @@ class CreateServer(graphene.Mutation):
             user_adding_right=server.user_adding_right,
         )
 
+
+class CreateMessage(graphene.Mutation):
+    text = graphene.String()
+    date = graphene.DateTime()
+    user = graphene.Field(UserType)
+    channel = graphene.Field(ChannelType)
+    id = graphene.Int()
+
+    class Arguments:
+        text = graphene.String()
+        user = graphene.Int()
+        channel = graphene.Int()
+
+    def mutate(self, info, text, user, channel):
+        message = Message(text=text, channel=Channel.objects.get(id=channel), user=get_user_model().objects.get(
+            id=user))
+        message.save()
+
+        return CreateMessage(
+            id=message.id,
+            text=message.text,
+            date=message.date
+        )
+
+
 class Mutation(graphene.ObjectType):
     create_server = CreateServer.Field()
+    create_user = CreateUser.Field()
+    create_message = CreateMessage.Field()
