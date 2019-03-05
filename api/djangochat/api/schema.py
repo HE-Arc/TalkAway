da45cd6e064d6c60f38ec5d79cc2b61f50b1afa5
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-03-05 23:16:45 +0100

    implemented basic authentication check when adding message

--- api/djangochat/api/schema.py
+++ api/djangochat/api/schema.py
@@ -1,12 +1,10 @@
 # djangochat/api/schema.py
 from django.contrib.auth import get_user_model
 import graphene
-
 from graphene_django.types import DjangoObjectType
-
 from .models import Message, Server, Channel, Friend, Right, Reaction
-
 from django.contrib.auth import get_user_model
+from graphql_jwt.decorators import login_required, user_passes_test
 
 
 class MessageType(DjangoObjectType):
@@ -87,26 +85,6 @@ class CreateUser(graphene.Mutation):
         )
 
 
-class CreateUser(graphene.Mutation):
-    user = graphene.Field(UserType)
-    
-    class Arguments:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
-        email = graphene.String(required=True)
-
-    def mutate(self, info, username, password, email):
-        user = get_user_model()(
-            username=username,
-            email=email,
-        )
-        user.set_password(password)
-        user.save()
-
-        return CreateUser(
-            user=user,
-        )
-
 class CreateServer(graphene.Mutation):
     id = graphene.Int()
     name = graphene.String()
@@ -116,6 +94,7 @@ class CreateServer(graphene.Mutation):
         name = graphene.String()
         user_adding_right = graphene.Int()
 
+    @login_required
     def mutate(self, info, name, user_adding_right):
         server = Server(name=name, user_adding_right=user_adding_right)
         server.save()
@@ -136,18 +115,27 @@ class CreateMessage(graphene.Mutation):
 
     class Arguments:
         text = graphene.String()
-        user = graphene.Int()
-        channel = graphene.Int()
+        user_id = graphene.Int()
+        channel_id = graphene.Int()
 
-    def mutate(self, info, text, user, channel):
-        message = Message(text=text, channel=Channel.objects.get(id=channel), user=get_user_model().objects.get(
-            id=user))
+    @login_required
+    def mutate(self, info, text, user_id, channel_id):
+        authUser = info.context.user
+        channel = Channel.objects.get(id=channel_id)
+
+        # or channel.user_one != None and channel.user_one != authUser.id and channel.user_two != authUser.id:
+        if authUser.id != user_id:
+            raise Exception('You are not correctly authentified')
+
+        message = Message(text=text, channel=channel, user=get_user_model().objects.get(
+            id=user_id))
         message.save()
 
         return CreateMessage(
             id=message.id,
             text=message.text,
-            date=message.date
+            date=message.date,
+            channel=message.channel
         )
 
 
