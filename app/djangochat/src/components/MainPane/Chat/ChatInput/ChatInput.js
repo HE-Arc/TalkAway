411234a4130ed97f4f82b4480715b6e552984d6f
Etiouse <etienne.husler@he-arc.ch>
2019-03-07 14:18:10 +0100

    Simple behavior for the text input

--- app/djangochat/src/components/MainPane/Chat/ChatInput/ChatInput.js
+++ app/djangochat/src/components/MainPane/Chat/ChatInput/ChatInput.js
@@ -7,12 +7,23 @@ class ChatInputComponent extends Component {
 
     constructor(props) {
         super(props);
+        this.updateArea = this.updateArea.bind(this);
+    }
+
+    updateArea() {
+        var area = this.refs.textArea;
+        area.style.height = 'inherit';
+        var scrollbarDisplay = area.clientHeight < area.scrollHeight;
+        if (scrollbarDisplay) {
+            var height = area.scrollHeight + 8;
+            area.style.height = height + "px";
+        }
     }
 
     render() {
         return (
             <div>
-                <textarea id="inputMessage" spellCheck="false"></textarea>
+                <textarea ref="textArea" id="inputMessage" spellCheck="false" onChange={this.updateArea}></textarea>
             </div>
         );
     }
